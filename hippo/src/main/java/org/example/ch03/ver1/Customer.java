fb2c27ac3c3c989c177c46193a03f9b734718698
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-01-25 21:01:06 +0900

    feat(ch04/데이터중심) : 데이터중심 설계 예시코드

---
+++ hippo/src/main/java/org/example/ch03/ver1/Customer.java
@@ -0,0 +1,11 @@
+package org.example.ch03.ver1;
+
+public class Customer {
+    private String name;
+    private String id;
+
+    public Customer(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}

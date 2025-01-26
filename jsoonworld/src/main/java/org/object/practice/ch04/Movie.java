44a8a3daac2a985105fa132d456197aade4a03e1
권장순 <jsoonworld@gmail.com>
2025-01-26 15:35:19 +0900

    feat(Movie) :접근 제어자, 수정자 적용

--- jsoonworld/src/main/java/org/object/practice/ch04/Movie.java
+++ jsoonworld/src/main/java/org/object/practice/ch04/Movie.java
@@ -1,5 +1,11 @@
 package org.object.practice.ch04;
 
+import org.object.practice.ch02.DiscountCondition;
+import org.object.practice.ch02.Money;
+
+import java.util.Collections;
+import java.util.List;
+
 public class Movie {
     private String title;
     private Duration runningTime;
@@ -9,4 +15,44 @@ public class Movie {
     private MovieType movieType;
     private Money discountAmount;
     private double discountPercent;
+
+    public MovieType getMovieType() {
+        return movieType;
+    }
+
+    public void setMovieType(MovieType movieType) {
+        this.movieType = movieType;
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public void setFee(Money fee) {
+        this.fee = fee;
+    }
+
+    public List<DiscountCondition> getDiscountConditions() {
+        return Collections.unmodifiableList(discountConditions);
+    }
+
+    public void setDiscountConditions(List<DiscountCondition> discountConditions) {
+        this.discountConditions = discountConditions;
+    }
+
+    public Money getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(Money discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public double getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public void setDiscountPercent(double discountPercent) {
+        this.discountPercent = discountPercent;
+    }
 }

3f5c41e7bdcb67a862918c3e685f8afe41a0605e
권장순 <jsoonworld@gmail.com>
2025-01-26 16:52:00 +0900

    refactor(Screening) : 데이터 책임 강화

--- jsoonworld/src/main/java/org/object/practice/ch04/Screening.java
+++ jsoonworld/src/main/java/org/object/practice/ch04/Screening.java
@@ -1,11 +1,39 @@
 package org.object.practice.ch04;
 
+import org.object.practice.ch02.Money;
+
 import java.time.LocalDateTime;
 
 public class Screening {
     private Movie movie;
     private int sequence;
     private LocalDateTime whenScreened;
+
+    public Screening(Movie movie, int sequence, LocalDateTime whenScreened) {
+        this.movie = movie;
+        this.sequence = sequence;
+        this.whenScreened = whenScreened;
+    }
+
+    public Money calculateFee(int audienceCount) {
+        switch (movie.getMovieType()) {
+            case AMOUNT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculateAmountDiscountedFee().times(audienceCount);
+                }
+                break;
+            case PERCENT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculatePercentDiscountedFee().times(audienceCount);
+                }
+                break;
+            case NONE_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculateNoneDiscountedFee().times(audienceCount);
+                }
+                break;
+        }
+    }
 
     public Movie getMovie() {
         return movie;

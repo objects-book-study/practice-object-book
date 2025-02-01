799b31342a68850cfe63b1d1402c89595ac87d84
minSsan <msp1125@hanyang.ac.kr>
2025-02-01 20:06:25 +0900

    refactor: TicketSeller와 Audience 사이 디미터 법칙 위반 해결

--- minsan/practice_ch06-/src/ch06/Audience.java
+++ minsan/practice_ch06-/src/ch06/Audience.java
@@ -7,7 +7,14 @@ public class Audience {
         this.bag = bag;
     }
 
-    public Bag getBag() {
-        return this.bag;
+    public Long setTicket(Ticket ticket) {
+        if (bag.hasInvitation()) {
+            bag.setTicket(ticket);
+            return 0L;
+        } else {
+            bag.minusAmount(ticket.getFee());
+            bag.setTicket(ticket);
+            return ticket.getFee();
+        }
     }
 }
